b99f8d5497fe3b1024910ea64fb8f2ecffa534e9
Zexi Hu <zexi@Zexis-MacBook-Pro.local>
2021-10-09 12:01:17 +0800

    add conda to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,9 @@ end
 neofetch
 alias cfggit '/usr/bin/git --git-dir=$HOME/.cfggit --work-tree=$HOME'
 set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
